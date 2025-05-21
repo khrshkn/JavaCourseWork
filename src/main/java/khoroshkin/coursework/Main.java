08601301dfa94a03c7de97c137cf2b6e2b961961
maksimka <khoroskinmaks@gmail.com>
2025-05-21 15:20:43 +0300

    solid with test

--- src/main/java/khoroshkin/coursework/Main.java
+++ src/main/java/khoroshkin/coursework/Main.java
@@ -8,35 +8,49 @@ import java.util.Arrays;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        int numThreads = 0, timeout = 0;
-        String[] services = new String[0];
-        String format = "";
         try {
-            if (args.length < 1) {
-                throw new IllegalArgumentException("Required format: java -cp src/main/java khoroshkin.coursework.Main <threads> <timeout> <service1> [service2...] <format>\n" +
-                        "Example: java -cp src/main/java khoroshkin.coursework.Main 3 20 spotify assembly virustotal json");
-            }
-            numThreads = Integer.parseInt(args[0]);
-            timeout = Integer.parseInt(args[1]);
-            if (numThreads <= 0 || timeout <= 0) {
-                throw new IllegalArgumentException("Threads and timeout must be greater than 0");
-            }
-
-            services = Arrays.copyOfRange(args, 2, args.length - 1);
-            format = args[args.length - 1];
-            if (services.length == 0) {
-                throw new IllegalArgumentException("Services must have at least one service");
-            }
-
-            if (!format.equalsIgnoreCase("json") && !format.equalsIgnoreCase("csv")) {
-                throw new IllegalArgumentException("Format must be either \"json\" or \"csv\"");
-            }
+            Arguments parsedArgs = processArguments(args);
+            APIScraper scraper = new APIScraper(parsedArgs.numThreads, parsedArgs.timeout, parsedArgs.services, parsedArgs.format);
+            scraper.start();
         } catch (IllegalArgumentException e) {
             logger.error("Invalid arguments provided", e);
             System.exit(1);
         }
-
-        APIScraper scraper = new APIScraper(numThreads, timeout, services, format);
-        scraper.start();
     }
+
+    static class Arguments {
+        int numThreads;
+        int timeout;
+        String[] services;
+        String format;
+
+        Arguments(int numThreads, int timeout, String[] services, String format) {
+            this.numThreads = numThreads;
+            this.timeout = timeout;
+            this.services = services;
+            this.format = format;
+        }
+    }
+
+    static Arguments processArguments(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Required format: java -cp src/main/java khoroshkin.coursework.Main <threads> <timeout> <service1> [service2...] <format>\n" +
+                    "Example: java -cp src/main/java khoroshkin.coursework.Main 3 20 spotify assembly virustotal json");
+        }
+        int numThreads = Integer.parseInt(args[0]);
+        int timeout = Integer.parseInt(args[1]);
+        if (numThreads <= 0 || timeout <= 0) {
+            throw new IllegalArgumentException("Threads and timeout must be integers and greater than 0");
+        }
+        String[] services = Arrays.copyOfRange(args, 2, args.length - 1);
+        String format = args[args.length - 1];
+        if (services.length == 0) {
+            throw new IllegalArgumentException("Services must have at least one service");
+        }
+        if (!format.equalsIgnoreCase("json") && !format.equalsIgnoreCase("csv")) {
+            throw new IllegalArgumentException("Format must be either \"json\" or \"csv\"");
+        }
+        return new Arguments(numThreads, timeout, services, format);
+    }
+
 }
